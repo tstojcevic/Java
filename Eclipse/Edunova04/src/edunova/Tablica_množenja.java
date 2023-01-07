f1a3519013bebc07bca4d0db83bb3ebe63eb05a5
Toni Stojčević <stojcevict@gmail.com>
2023-01-07 20:06:19 +0100

    unos linije ispisa autora tablice

--- Eclipse/Edunova04/src/edunova/Tablica_množenja.java
+++ Eclipse/Edunova04/src/edunova/Tablica_množenja.java
@@ -1,20 +1,24 @@
 package edunova;
 
+import javax.swing.JOptionPane;
+
 public class Tablica_množenja {
 	
 	public static void main(String[] args) {
 		
+		String ime = JOptionPane.showInputDialog("Tko je kreirao tablicu?");
+		
 		System.out.println("-------------------------------");
 		System.out.println(": : :  TABLICA  MNOZENJA  : : :");
 		System.out.println("-------------------------------");
-		System.out.println("*  | 1  2  3  4  5  6  7  8  9 ");
+		System.out.println("*  |  1  2  3  4  5  6  7  8  9 ");
 		System.out.println("-------------------------------");
 	
 	for (int i=1; i<=9; i++) {
 		for (int j=1; j<=9; j++) {
-			if(i * j <10) {
+			if(i * j <=9) {
 				if (j==1) {
-					System.out.print(i + "  | " + i * j + "  ");
+					System.out.print(i + "  | " + i * j + "  " );
 				} else {
 					System.out.print(i * j + "  ");
 				}				
@@ -29,5 +33,41 @@ public class Tablica_množenja {
 		} System.out.println();
 		
 }System.out.println("-------------------------------");
+
+  switch (ime.length()) {
+  case 2:
+		System.out.println(":  :  :  :  :  :   :  :  :by " + ime);
+		break;
+
+  case 3:
+		System.out.println(":  :  :  :  :  :   :  :  by " + ime);
+		break;
+
+  case 4:
+		System.out.println(":  :  :  :  :  :   :  : by " + ime);
+		break;
+  case 5:
+		System.out.println(":  :  :  :  :  :   :  :by " + ime);
+		break;
+  case 6:
+		System.out.println(":  :  :  :  :  :   :  by " + ime);
+		break;
+  case 7:
+		System.out.println(":  :  :  :  :  :   : by " + ime);
+		break;
+  case 8:
+		System.out.println(":  :  :  :  :  :   :by " + ime);
+		break;
+  case 9:
+		System.out.println(":  :  :  :  :  :   by " + ime);
+		break;
+  case 10:
+		System.out.println(":  :  :  :  :  :  by " + ime);
+		break;
+  case 11:
+		System.out.println(":  :  :  :  :  : by " + ime);
+		break;
+		
+  }System.out.println("-------------------------------");
 }
 }
