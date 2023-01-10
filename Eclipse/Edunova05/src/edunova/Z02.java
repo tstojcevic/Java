bfdb1bd71e4ae4e6c6bb001fbba1949b2a9a8652
Toni Stojčević <stojcevict@gmail.com>
2023-01-10 20:37:47 +0100

    riješen drugi zadatak

---
+++ Eclipse/Edunova05/src/edunova/Z02.java
@@ -0,0 +1,27 @@
+package edunova;
+
+import javax.swing.JOptionPane;
+
+public class Z02 {
+	
+	public static void main(String[] args) {
+		
+		// Korisnik unosi 5 brojeva
+		// između 2 i 4
+		// Program ispisuje njihov umnožak
+		
+		int umnozak = 1;
+		int broj, brojac=0;
+		
+		while (brojac<5) {
+			brojac = Integer.parseInt(JOptionPane.showInputDialog("Unesite 5 brojeva između 2 i 4"));
+			
+			if (broj>=2 && broj<=4) {
+				umnozak*=broj;
+				brojac++;
+			}System.out.println(umnozak);
+		}
+		
+	}
+
+}
