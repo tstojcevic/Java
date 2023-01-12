457fd46251e97f5744867373e726b9d0d4a5726e
Toni Stojčević <stojcevict@gmail.com>
2023-01-12 19:07:26 +0100

    Prvi zadatak riješen

---
+++ Eclipse/Edunova06/src/edunova/Z01.java
@@ -0,0 +1,19 @@
+package edunova;
+
+public class Z01 {
+	
+	// Napišite metodu koja prima
+		// tri cijela broja i vraća najveći
+	
+	public static int najveci (int a, int b, int c) {
+		
+		if (a>b && a>c) {
+			return a;	
+		}
+		if (b>a && b>c) {
+			return b;
+		}
+		return c;
+	}
+
+}
