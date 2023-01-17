4134afd8582623a401d7c064dd6769c0d8ac562e
Toni Stojčević <stojcevict@gmail.com>
2023-01-17 18:23:02 +0100

    Nije dobro, početi ispočetka

--- Eclipse/Edunova04/src/edunova/CiklicnaMatrica.java
+++ Eclipse/Edunova04/src/edunova/CiklicnaMatrica.java
@@ -20,43 +20,9 @@ public class CiklicnaMatrica {
 	int zadnjiStupac = stupac - 1;
 	int prviRed = 0;
 	int zadnjiRed = red - 1;
+	
+	
 
-	int polje = 1;
-
-	while(polje <= stupac*red){
-
-		for (int i = zadnjiStupac; i >= prviStupac; i--) {
-			matrica[zadnjiRed][i] = polje++;
-		}
-		for (int i = zadnjiRed - 1; i >= prviRed; i--) {
-			matrica[i][prviStupac] = polje++;
-		}
-		for (int i = prviStupac + 1; i <= zadnjiStupac; i++) {
-			matrica[prviRed][i] = polje++;
-		}
-		for (int i = prviRed + 1; i <= zadnjiRed - 1; i++) {
-			matrica[i][zadnjiStupac] = polje++;
-		}
-		
-		zadnjiRed = zadnjiRed - 1;
-		zadnjiStupac = zadnjiStupac - 1;
-		
-		prviStupac = prviStupac + 1;
-		prviRed = prviRed + 1;
-		
-		
-
-			}for (int i = 0; i < stupac; i++) {
-
-				for (int j = 0; j < red; j++) {
-
-					System.out.print(matrica[i][j] + " \t ");
-
-			
-		}
-		System.out.println();
-
-	}
 
 }
 	}
