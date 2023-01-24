1dd0e8ac861b3c856a1bb108738909628405dfa7
Toni Stojčević <stojcevict@gmail.com>
2023-01-24 19:12:37 +0100

    Kreirana klasa Kupac

---
+++ Eclipse/Edunova08/src/edunova/Kupac.java
@@ -0,0 +1,27 @@
+package edunova;
+
+public class Kupac {
+	
+	// Kupac je klasa koja ima svojstva
+		// 1. naziv
+		// 2. adresa
+	
+	private String naziv;
+	private String adresa;
+	
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public String getAdresa() {
+		return adresa;
+	}
+	public void setAdresa(String adresa) {
+		this.adresa = adresa;
+	}
+	
+	
+
+}
