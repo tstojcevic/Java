41b9d0f3cfc0c9817ba6f038a063fc243ff61c68
Toni Stojčević <stojcevict@gmail.com>
2023-02-19 18:02:05 +0100

    riješen zadatak sa ispisom

--- netbeans/BNZ1/src/main/java/stojcevic/ProzorDrugi.java
+++ netbeans/BNZ1/src/main/java/stojcevic/ProzorDrugi.java
@@ -29,13 +29,21 @@ public class ProzorDrugi extends javax.swing.JFrame {
         btnProzorDrugi = new javax.swing.JButton();
         lblTekst = new javax.swing.JLabel();
         txtUnos = new javax.swing.JTextField();
+        btnZatvori = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
-        btnProzorDrugi.setText("Unesi imena za ispis ");
+        btnProzorDrugi.setText("Ispis unesenih podataka");
         btnProzorDrugi.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnProzorDrugiActionPerformed(evt);
+            }
+        });
+
+        btnZatvori.setText("Zatvori");
+        btnZatvori.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnZatvoriActionPerformed(evt);
             }
         });
 
@@ -45,37 +53,47 @@ public class ProzorDrugi extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(43, 43, 43)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(lblTekst, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(btnProzorDrugi)
-                        .addGap(62, 62, 62)
-                        .addComponent(txtUnos, javax.swing.GroupLayout.DEFAULT_SIZE, 151, Short.MAX_VALUE)))
-                .addContainerGap())
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(txtUnos, javax.swing.GroupLayout.DEFAULT_SIZE, 233, Short.MAX_VALUE)
+                    .addComponent(btnProzorDrugi)
+                    .addComponent(lblTekst, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(124, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnZatvori)
+                .addGap(32, 32, 32))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(66, 66, 66)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnProzorDrugi, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(txtUnos, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(42, 42, 42)
+                .addGap(32, 32, 32)
+                .addComponent(txtUnos, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(33, 33, 33)
+                .addComponent(btnProzorDrugi, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 37, Short.MAX_VALUE)
                 .addComponent(lblTekst, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(108, Short.MAX_VALUE))
+                .addGap(24, 24, 24)
+                .addComponent(btnZatvori, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(20, 20, 20))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnProzorDrugiActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnProzorDrugiActionPerformed
-        dispose();
+            lblTekst.setText(txtUnos.getText());
+        
     }//GEN-LAST:event_btnProzorDrugiActionPerformed
+
+    private void btnZatvoriActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnZatvoriActionPerformed
+        dispose();
+    }//GEN-LAST:event_btnZatvoriActionPerformed
 
    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnProzorDrugi;
+    private javax.swing.JButton btnZatvori;
     private javax.swing.JLabel lblTekst;
     private javax.swing.JTextField txtUnos;
     // End of variables declaration//GEN-END:variables
