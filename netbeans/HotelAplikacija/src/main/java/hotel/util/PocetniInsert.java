46d900b8c01c092b05a0c522bc0942f4b6a953b5
Toni Stojčević <stojcevict@gmail.com>
2023-03-04 19:22:59 +0100

    kreirana klasa PocetniInsert

---
+++ netbeans/HotelAplikacija/src/main/java/hotel/util/PocetniInsert.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hotel.util;
+
+import org.hibernate.Session;
+
+/**
+ *
+ * @author Korisnik
+ */
+public class PocetniInsert {
+    
+    Session session = HibernateUtil.getSession();
+    
+}
