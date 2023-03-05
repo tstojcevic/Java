85802aa39440ab0ac3f9852e917a835abb730360
Toni Stojčević <stojcevict@gmail.com>
2023-03-05 15:54:25 +0100

    dodani gosti

--- netbeans/HotelAplikacija/src/main/java/hotel/util/PocetniInsert.java
+++ netbeans/HotelAplikacija/src/main/java/hotel/util/PocetniInsert.java
@@ -4,6 +4,10 @@
  */
 package hotel.util;
 
+import com.github.javafaker.Faker;
+import hotel.model.Gost;
+import java.util.ArrayList;
+import java.util.List;
 import org.hibernate.Session;
 
 /**
@@ -12,6 +16,43 @@ import org.hibernate.Session;
  */
 public class PocetniInsert {
     
-    Session session = HibernateUtil.getSession();
+    private static final int BROJ_GOSTIJU = 20;
+    
+    private Faker faker;
+    private List<Gost> gosti;
+    
+    private Session session;
+
+    public PocetniInsert() {
+        
+        faker = new Faker();
+        gosti = new ArrayList<>();
+        
+        session = HibernateUtil.getSession();
+        session.beginTransaction();
+        
+        kreirajGoste();
+        
+        session.getTransaction().commit();
+    }
+
+    private void kreirajGoste() {
+        
+        Gost g;
+        for (int i = 0; i < BROJ_GOSTIJU; i++){
+            g =new Gost();
+            g.setIme(faker.name().firstName());
+            g.setPrezime(faker.name().lastName());
+            g.setOIB(Alati.dovuciOib());
+            g.setEmail(faker.internet().emailAddress());
+            g.setKontakTelefon(faker.phoneNumber().cellPhone());
+            
+            session.persist(g);
+            gosti.add(g);
+        }
+        
+    }
+    
+    
       
 }
