c9cea472d8c197d13aaa0af259a122e8a3e19359
Toni Stojčević <stojcevict@gmail.com>
2023-03-06 10:38:19 +0100

    dodani smještaji

--- netbeans/HotelAplikacija/src/main/java/hotel/util/PocetniInsert.java
+++ netbeans/HotelAplikacija/src/main/java/hotel/util/PocetniInsert.java
@@ -7,6 +7,9 @@ package hotel.util;
 import com.github.javafaker.Faker;
 import hotel.model.Djelatnik;
 import hotel.model.Gost;
+import hotel.model.RadnoMjesto;
+import hotel.model.Smjestaj;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import org.hibernate.Session;
@@ -16,64 +19,105 @@ import org.hibernate.Session;
  * @author Korisnik
  */
 public class PocetniInsert {
-    
+
     private static final int BROJ_GOSTIJU = 50;
+    private static final int BROJ_RADNIH_MJESTA = 5;
+    private static final int BROJ_SMJESTAJA = 20;
     private static final int BROJ_DJELATNIKA = 20;
     
+
     private Faker faker;
     private List<Gost> gosti;
+    private List<RadnoMjesto> radna_mjesta;
+    private List<Smjestaj> smjestaji;
     private List<Djelatnik> djelatnici;
-    
+
     private Session session;
 
     public PocetniInsert() {
-        
+
         faker = new Faker();
         gosti = new ArrayList<>();
+        radna_mjesta = new ArrayList<>();
+        smjestaji = new ArrayList<>();
         djelatnici = new ArrayList<>();
-        
-        
+
         session = HibernateUtil.getSession();
         session.beginTransaction();
-        
+
         kreirajGoste();
+        kreirajRadnaMjesta();
+        kreirajSmjestaje();
         kreirajDjelatnike();
-        
+
         session.getTransaction().commit();
     }
 
     private void kreirajGoste() {
-        
+
         Gost g;
-        for (int i = 0; i < BROJ_GOSTIJU; i++){
-            g =new Gost();
+        for (int i = 0; i < BROJ_GOSTIJU; i++) {
+            g = new Gost();
             g.setIme(faker.name().firstName());
             g.setPrezime(faker.name().lastName());
             g.setOIB(Alati.dovuciOib());
             g.setEmail(faker.internet().emailAddress());
             g.setKontakTelefon(faker.phoneNumber().cellPhone());
-            
+
             session.persist(g);
             gosti.add(g);
         }
-        
+
     }
 
-    private void kreirajDjelatnike() {
+    private void kreirajRadnaMjesta() {
+
+        RadnoMjesto rm;
+        for (int i = 0; i < BROJ_RADNIH_MJESTA; i++) {
+            rm = new RadnoMjesto();
+            rm.setNaziv(faker.job().position());
+            rm.setPlaca(new BigDecimal(faker.number().numberBetween(800, 2500)));
+            rm.setSmjenskiRad(faker.bool().bool());
+            
+            session.persist(rm);
+            radna_mjesta.add(rm);
+        }
+
+    }
+    
+    
+    private void kreirajSmjestaje() {
         
+        Smjestaj s;
+        for (int i = 0; i < BROJ_SMJESTAJA; i++){
+            s =new Smjestaj();
+            s.setVrsta(faker.toString().toString());
+            s.setBrojKreveta(faker.number().numberBetween(1, 3));
+            s.setCijena(new BigDecimal(faker.number().numberBetween(80, 200)));
+            s.setRaspolozivost(faker.bool().bool());
+            
+            session.persist(s);
+            smjestaji.add(s);
+        }
+    }
+    
+    
+
+    private void kreirajDjelatnike() {
+
         Djelatnik d;
-        for (int i = 0; i < BROJ_DJELATNIKA; i++){
-            d=new Djelatnik();
+        for (int i = 0; i < BROJ_DJELATNIKA; i++) {
+            d = new Djelatnik();
             d.setIme(faker.name().firstName());
             d.setPrezime(faker.name().lastName());
             d.setBrojUgovora(faker.number().toString());
             d.setOIB(Alati.dovuciOib());
-            
+
             session.persist(d);
             djelatnici.add(d);
         }
     }
+
     
-    
-      
+
 }
