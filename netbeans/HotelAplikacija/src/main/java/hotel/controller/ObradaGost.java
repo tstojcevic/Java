57c0457c7b6de1a4df316043eb58e3e17fd1ae8a
Toni Stojčević <stojcevict@gmail.com>
2023-03-07 21:09:37 +0100

    završen kontroler za klasu Gost

--- netbeans/HotelAplikacija/src/main/java/hotel/controller/ObradaGost.java
+++ netbeans/HotelAplikacija/src/main/java/hotel/controller/ObradaGost.java
@@ -5,6 +5,7 @@
 package hotel.controller;
 
 import hotel.model.Gost;
+import hotel.util.Alati;
 import hotel.util.HotelException;
 import java.util.List;
 
@@ -21,22 +22,164 @@ public class ObradaGost extends Obrada<Gost>{
 
     @Override
     protected void kontrolaUnos() throws HotelException {
+        kontrolaIme();
+        kontrolaPrezime();
+        kontrolaOIB();
+        kontrolaEmail();
+        kontrolaKontaktTelefon();
         
     }
 
     @Override
     protected void kontrolaPromjena() throws HotelException {
-        
+        kontrolaImeNull();
+            kontrolaImeBroj();
+            kontrolaImeMinimalnaDuzina();
+            kontrolaImeMaksimalnaDuzina();
+            kontrolaImeSadrziBroj();
+            
+            kontrolaPrezimeNull();
+            kontrolaPrezimeBroj();
+            kontrolaPrezimeMinimalnaDuzina();
+            kontrolaPrezimeMaksimalnaDuzina();
+            kontrolaPrezimeSadrziBroj();
+            
+            kontrolaOIB();
+            
+            kontrolaEmailNull();
+            
+            kontrolaKontaktTelefonNull();
     }
 
     @Override
     protected void kontrolaBrisanje() throws HotelException {
         
     }
+
+    private void kontrolaIme() throws HotelException{
+            kontrolaImeNull();
+            kontrolaImeBroj();
+            kontrolaImeMinimalnaDuzina();
+            kontrolaImeMaksimalnaDuzina();
+            kontrolaImeSadrziBroj();
+    }
+
+    private void kontrolaImeNull() throws HotelException{
+        
+        if(entitet.getIme()==null){
+            throw new HotelException("Ime mora biti upisano");
+        }
+    }
+
+    private void kontrolaImeBroj() throws HotelException{
+        boolean broj = false;
+        try {
+            Double.parseDouble(entitet.getIme());
+            broj = true;
+        } catch (Exception e) {
+        }
+        if (broj){
+            throw new HotelException("Ime gosta ne smije biti broj");
+        }
+    }
+
+    private void kontrolaImeMinimalnaDuzina() throws HotelException{
+        if(entitet.getIme().trim().length() < 3){
+            throw new HotelException("Ime gosta može imati minimalno 3 znaka");
+        }
+    }
+
+    private void kontrolaImeMaksimalnaDuzina() throws HotelException{
+        if(entitet.getIme().trim().length() > 20){
+            throw new HotelException("Ime gosta može imati maksimalno 20 znakova");
+    }
+    } 
     
-    
-    
-    
-    
-    
+    private void kontrolaImeSadrziBroj() throws HotelException{
+        
+        char[] b = entitet.getIme().toCharArray();
+        
+        for (char ch : b) {
+            if (Character.isDigit(ch)){
+                throw new HotelException("Ime sadrži broj");
+            }
+                    
+    }
+}
+
+      private void kontrolaPrezime() throws HotelException{
+            kontrolaPrezimeNull();
+            kontrolaPrezimeBroj();
+            kontrolaPrezimeMinimalnaDuzina();
+            kontrolaPrezimeMaksimalnaDuzina();
+            kontrolaPrezimeSadrziBroj();
+    }
+
+
+
+    private void kontrolaPrezimeNull() throws HotelException{
+        if(entitet.getPrezime()==null){
+            throw new HotelException("Prezime mora biti upisano");
+        }
+    }
+
+    private void kontrolaPrezimeBroj() throws HotelException{
+        boolean broj = false;
+        try {
+            Double.parseDouble(entitet.getPrezime());
+            broj = true;
+        } catch (Exception e) {
+        }
+        if (broj){
+            throw new HotelException("Prezime gosta ne smije biti broj");
+        }
+    }
+
+    private void kontrolaPrezimeMinimalnaDuzina() throws HotelException{
+        if(entitet.getPrezime().trim().length() < 3){
+            throw new HotelException("Prezime gosta može imati minimalno 3 znaka");
+        }
+    }
+
+    private void kontrolaPrezimeMaksimalnaDuzina() throws HotelException{
+        if(entitet.getPrezime().trim().length() > 20){
+            throw new HotelException("Prezime gosta može imati maksimalno 30 znakova");
+    }
+}
+        private void kontrolaPrezimeSadrziBroj() throws HotelException{
+        char[] b = entitet.getIme().toCharArray();
+        for (char ch : b) {
+            if (Character.isDigit(ch)){
+                throw new HotelException("Prezime sadrži broj");
+            }
+    }
+}
+
+    private void kontrolaOIB() throws HotelException{
+        if(Alati.kontrolaOIB(entitet.getOIB())){
+                throw new HotelException("OIB nije u dobrom formatu");
+    }
+}
+
+    private void kontrolaEmail() throws HotelException{
+            kontrolaEmailNull();
+    }
+
+    private void kontrolaEmailNull() throws HotelException{
+        if (entitet.getEmail()==null){
+            throw new HotelException("Email mora biti unesen");
+        }
+    }
+
+    private void kontrolaKontaktTelefon() throws HotelException{
+        kontrolaKontaktTelefonNull();
+    }
+
+    private void kontrolaKontaktTelefonNull() throws HotelException{
+        if (entitet.getKontakTelefon()==null){
+            throw new HotelException("Kontakt telefon mora biti unesen");
+        }
+    }
+
+
 }
