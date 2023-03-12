473cf07880250e4740ff1fe52dc3820533c718c3
Toni Stojčević <stojcevict@gmail.com>
2023-03-12 12:33:25 +0100

    uređen kontroler

--- netbeans/HotelAplikacija/src/main/java/hotel/controller/ObradaGost.java
+++ netbeans/HotelAplikacija/src/main/java/hotel/controller/ObradaGost.java
@@ -13,11 +13,11 @@ import java.util.List;
  *
  * @author Korisnik
  */
-public class ObradaGost extends Obrada<Gost>{
+public class ObradaGost extends Obrada<Gost> {
 
     @Override
     public List<Gost> read() {
-        return session.createQuery("from Gost", Gost.class).list();
+        return session.createQuery("from Gost order by ime", Gost.class).list();
     }
 
     @Override
@@ -27,159 +27,163 @@ public class ObradaGost extends Obrada<Gost>{
         kontrolaOIB();
         kontrolaEmail();
         kontrolaKontaktTelefon();
-        
+
     }
 
     @Override
     protected void kontrolaPromjena() throws HotelException {
-            kontrolaImeNull();
-            kontrolaImeBroj();
-            kontrolaImeMinimalnaDuzina();
-            kontrolaImeMaksimalnaDuzina();
-            kontrolaImeSadrziBroj();
-            
-            kontrolaPrezimeNull();
-            kontrolaPrezimeBroj();
-            kontrolaPrezimeMinimalnaDuzina();
-            kontrolaPrezimeMaksimalnaDuzina();
-            kontrolaPrezimeSadrziBroj();
-            
-            kontrolaOIB();
-            
-            kontrolaEmailNull();
-            
-            kontrolaKontaktTelefonNull();
+        kontrolaImeNull();
+        kontrolaImeBroj();
+        kontrolaImeMinimalnaDuzina();
+        kontrolaImeMaksimalnaDuzina();
+        kontrolaImeSadrziBroj();
+
+        kontrolaPrezimeNull();
+        kontrolaPrezimeBroj();
+        kontrolaPrezimeMinimalnaDuzina();
+        kontrolaPrezimeMaksimalnaDuzina();
+        kontrolaPrezimeSadrziBroj();
+
+        kontrolaOIBNull();
+
+        kontrolaEmailNull();
+
+        kontrolaKontaktTelefonNull();
     }
 
     @Override
     protected void kontrolaBrisanje() throws HotelException {
-        
+
     }
 
-    private void kontrolaIme() throws HotelException{
-            kontrolaImeNull();
-            kontrolaImeBroj();
-            kontrolaImeMinimalnaDuzina();
-            kontrolaImeMaksimalnaDuzina();
-            kontrolaImeSadrziBroj();
+    private void kontrolaIme() throws HotelException {
+        kontrolaImeNull();
+        kontrolaImeBroj();
+        kontrolaImeMinimalnaDuzina();
+        kontrolaImeMaksimalnaDuzina();
+        kontrolaImeSadrziBroj();
     }
 
-    private void kontrolaImeNull() throws HotelException{
-        
-        if(entitet.getIme()==null){
+    private void kontrolaImeNull() throws HotelException {
+
+        if (entitet.getIme() == null) {
             throw new HotelException("Ime mora biti upisano");
         }
     }
 
-    private void kontrolaImeBroj() throws HotelException{
+    private void kontrolaImeBroj() throws HotelException {
         boolean broj = false;
         try {
             Double.parseDouble(entitet.getIme());
             broj = true;
         } catch (Exception e) {
         }
-        if (broj){
+        if (broj) {
             throw new HotelException("Ime gosta ne smije biti broj");
         }
     }
 
-    private void kontrolaImeMinimalnaDuzina() throws HotelException{
-        if(entitet.getIme().trim().length() < 3){
+    private void kontrolaImeMinimalnaDuzina() throws HotelException {
+        if (entitet.getIme().trim().length() < 3) {
             throw new HotelException("Ime gosta može imati minimalno 3 znaka");
         }
     }
 
-    private void kontrolaImeMaksimalnaDuzina() throws HotelException{
-        if(entitet.getIme().trim().length() > 20){
+    private void kontrolaImeMaksimalnaDuzina() throws HotelException {
+        if (entitet.getIme().trim().length() > 20) {
             throw new HotelException("Ime gosta može imati maksimalno 20 znakova");
+        }
     }
-    } 
-    
-    private void kontrolaImeSadrziBroj() throws HotelException{
-        
+
+    private void kontrolaImeSadrziBroj() throws HotelException {
+
         char[] b = entitet.getIme().toCharArray();
-        
+
         for (char ch : b) {
-            if (Character.isDigit(ch)){
+            if (Character.isDigit(ch)) {
                 throw new HotelException("Ime sadrži broj");
             }
-                    
-    }
-}
 
-      private void kontrolaPrezime() throws HotelException{
-            kontrolaPrezimeNull();
-            kontrolaPrezimeBroj();
-            kontrolaPrezimeMinimalnaDuzina();
-            kontrolaPrezimeMaksimalnaDuzina();
-            kontrolaPrezimeSadrziBroj();
+        }
     }
 
+    private void kontrolaPrezime() throws HotelException {
+        kontrolaPrezimeNull();
+        kontrolaPrezimeBroj();
+        kontrolaPrezimeMinimalnaDuzina();
+        kontrolaPrezimeMaksimalnaDuzina();
+        kontrolaPrezimeSadrziBroj();
+    }
 
-
-    private void kontrolaPrezimeNull() throws HotelException{
-        if(entitet.getPrezime()==null){
+    private void kontrolaPrezimeNull() throws HotelException {
+        if (entitet.getPrezime() == null) {
             throw new HotelException("Prezime mora biti upisano");
         }
     }
 
-    private void kontrolaPrezimeBroj() throws HotelException{
+    private void kontrolaPrezimeBroj() throws HotelException {
         boolean broj = false;
         try {
             Double.parseDouble(entitet.getPrezime());
             broj = true;
         } catch (Exception e) {
         }
-        if (broj){
+        if (broj) {
             throw new HotelException("Prezime gosta ne smije biti broj");
         }
     }
 
-    private void kontrolaPrezimeMinimalnaDuzina() throws HotelException{
-        if(entitet.getPrezime().trim().length() < 3){
+    private void kontrolaPrezimeMinimalnaDuzina() throws HotelException {
+        if (entitet.getPrezime().trim().length() < 3) {
             throw new HotelException("Prezime gosta može imati minimalno 3 znaka");
         }
     }
 
-    private void kontrolaPrezimeMaksimalnaDuzina() throws HotelException{
-        if(entitet.getPrezime().trim().length() > 20){
+    private void kontrolaPrezimeMaksimalnaDuzina() throws HotelException {
+        if (entitet.getPrezime().trim().length() > 20) {
             throw new HotelException("Prezime gosta može imati maksimalno 30 znakova");
+        }
     }
-}
-        private void kontrolaPrezimeSadrziBroj() throws HotelException{
+
+    private void kontrolaPrezimeSadrziBroj() throws HotelException {
         char[] b = entitet.getIme().toCharArray();
         for (char ch : b) {
-            if (Character.isDigit(ch)){
+            if (Character.isDigit(ch)) {
                 throw new HotelException("Prezime sadrži broj");
             }
-    }
-}
-
-    private void kontrolaOIB() throws HotelException{
-        if(Alati.kontrolaOIB(entitet.getOIB())){
-                throw new HotelException("OIB nije u dobrom formatu");
-    }
-}
-
-    private void kontrolaEmail() throws HotelException{
-            kontrolaEmailNull();
+        }
     }
 
-    private void kontrolaEmailNull() throws HotelException{
-        if (entitet.getEmail()==null){
+    private void kontrolaOIB() throws HotelException {
+        kontrolaOIBNull();
+    }
+
+    private void kontrolaOIBNull() throws HotelException {
+        if (entitet.getOIB() == null) {
+            throw new HotelException("OIB mora biti unesen");
+        } else if (!Alati.kontrolaOIB(entitet.getOIB())) {
+            throw new HotelException("OIB nije u dobrom formatu");
+        }
+    }
+
+    private void kontrolaEmail() throws HotelException {
+        kontrolaEmailNull();
+    }
+
+    private void kontrolaEmailNull() throws HotelException {
+        if (entitet.getEmail() == null) {
             throw new HotelException("Email mora biti unesen");
         }
     }
 
-    private void kontrolaKontaktTelefon() throws HotelException{
+    private void kontrolaKontaktTelefon() throws HotelException {
         kontrolaKontaktTelefonNull();
     }
 
-    private void kontrolaKontaktTelefonNull() throws HotelException{
-        if (entitet.getKontakTelefon()==null){
+    private void kontrolaKontaktTelefonNull() throws HotelException {
+        if (entitet.getKontakTelefon() == null) {
             throw new HotelException("Kontakt telefon mora biti unesen");
         }
     }
-
 
 }
