5645363592cfb7a9f1ccbbbcd5b4041f10b83f9e
Toni Stojčević <stojcevict@gmail.com>
2023-03-13 20:38:08 +0100

    unos imena kroz kontruktor

--- Eclipse/LjubavniKalkulator/src/stojcevic/Kalkulator.java
+++ Eclipse/LjubavniKalkulator/src/stojcevic/Kalkulator.java
@@ -1,5 +1,32 @@
 package stojcevic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Kalkulator {
+	
+	public static void main(String[] args) {
+		
+		new Kalkulator();
+		
+	}
+	
+	public Kalkulator () {
+		
+		String ime1, ime2;
+		String imenaZajedno;
+		
+		//pohrana znakova
+		List<Character> nizZnakova = new ArrayList<Character>();
+		//pohrana brojeva
+		List<Integer> nizBrojeva = new ArrayList<Integer>();
+		
+		ime1 = Pomocno.Unos("Unesi prvo ime").replaceAll("\\s", "");
+		
+		ime2 = Pomocno.Unos("Unesi drugo ime").replaceAll("\\s", "");
+		
+		
+		
+	}
 
 }
