bf1d86911c05a296eea46d87f486be4ced8a1778
Toni Stojčević <stojcevict@gmail.com>
2023-03-21 20:44:12 +0100

    pocetak kontrola za rezervacije

--- netbeans/HotelAplikacija/src/main/java/hotel/controller/ObradaRezervacija.java
+++ netbeans/HotelAplikacija/src/main/java/hotel/controller/ObradaRezervacija.java
@@ -4,10 +4,36 @@
  */
 package hotel.controller;
 
+import hotel.model.Rezervacija;
+import hotel.util.HotelException;
+import java.util.List;
+
 /**
  *
  * @author Korisnik
  */
-public class ObradaRezervacija {
+public class ObradaRezervacija extends Obrada<Rezervacija>{
+
+    @Override
+    public List<Rezervacija> read() {
+        return session.createQuery("from Rezervacija order by brojRezervacije", Rezervacija.class).list();
+    }
+
+    @Override
+    protected void kontrolaUnos() throws HotelException {
+        
+    }
+
+    @Override
+    protected void kontrolaPromjena() throws HotelException {
+        
+    }
+
+    @Override
+    protected void kontrolaBrisanje() throws HotelException {
+        
+    }
+    
+    
     
 }
